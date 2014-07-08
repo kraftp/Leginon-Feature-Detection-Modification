More correctly updated conversion code to fix normalization bugs.  Tested and works.

leginon/openCVcaller.py
@@ -94,7 +94,7 @@ def MatchImages(image1, image2, blur=3):
     print "%d matches" % (len(sel_matches))
     
     count=0
-    while len(sel_matches)<40 and count<5:
+    while len(sel_matches)<40 and count<10:
         count+=1
         sel_matches = [m for m in matches if m.distance < mean_dist*(.6+.05*count)]
         ys=yshift(k1, k2, sel_matches)
@@ -162,17 +162,26 @@ def convertImage(image1):
         numpy image1 array, dtype=uint8
         numpy image2 array, dtype=uint8   
     """
+    #print image1
+    
     max1 = np.amax(image1)
 
     min1 = np.amin(image1)
   
+    #print min1, max1
 
-    image1 = image1*256/(max1-min1)
+    image1 = image1 - min1
+    
+    max1 = np.amax(image1)
 
+   # print max1
+    
+    image1 = image1/max1 * 256
 
     image1 = np.asarray(image1, dtype=np.uint8)
 
-
+    #print image1
+    
     return image1
 
 #-----------------------